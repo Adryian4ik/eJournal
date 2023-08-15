841486d75a62a39bbdea31a759922f2cb4216f06
Adryian <adrian_zare@mail.ru>
2023-08-16 01:00:55 +0300

    add function to get students list of group

--- main.py
+++ main.py
@@ -47,18 +47,40 @@ async def is_elder(request: fastapi.Request):
 
 
 
-    if params.get("id", None) or params.get("code", None):
+    if params.get("id", None) and params.get("code", None):
         group = cipher_suite.decrypt(params["code"]).decode()
+        cursor.row_factory = sqlite3.Row
         print(group)
         cursor.execute("SELECT * FROM Grupp WHERE bossId=={0} AND name==\"{1}\"".format(params["id"], group))
         db.commit()
         db_answer = cursor.fetchall()
         print(db_answer)
         if len(db_answer) != 0:
-            return {"type": "answer", "answer": True, "groupName": db_answer[0][2]}
+            return {"type": "answer", "answer": True, "groupName": db_answer[0]["name"], "groupId":db_answer[0]["id"]}
         # except:
         #     print("error")
     return {"type":"error"}
+
+
+@app.get("/getgroupinfo")
+async def api_data(request: fastapi.Request):
+    params = request.query_params
+    if params.get("groupId", None):
+        gId = int(params["groupId"])
+        cursor.row_factory = sqlite3.Row
+        cursor.execute("""SELECT * FROM Students WHERE groupId=={0};""".format(gId))
+        db.commit()
+        return {"type": "answer", "data":cursor.fetchall()}
+        # try:
+        #     cursor.execute(params["command"])
+        #     db.commit()
+        #     return cursor.fetchall()
+        # except:
+        #     print("error")
+    return  {"type": "error"}
+
+
+
 
 
 # def main():
